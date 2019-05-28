a7837484afcd556412c4b083f0dc591ea56efe82
Ishan Bhatt <ishanbhatt@halwa>
2019-05-28 11:28:21 +0530

    Basic groupingBy construct with comment

--- src/Chapter6/CollectStream.java
+++ src/Chapter6/CollectStream.java
@@ -9,11 +9,19 @@ import java.util.stream.Collectors;
 import static java.util.stream.Collectors.*;
 
 public class CollectStream {
+
     /*Collectors class
     1) Reducing and summarizing stream elements to a single value
     2) Grouping elements
     3) Partitioning elements
+    All the arguments to  collect method are toList, reducing, groupingBy, joining here.
+    All those methods return type is collector interface (Or Some implementation of Collector Interface)
+    All of them are defined in  collectors class
+    collect method is in Stream class, that can take any argument of type collector interface
     * */
+
+    public enum CaloricLevel {DIET, NORMAL, FAT}
+
     public static void main(String[] args) {
         List<Dish> menu = Arrays.asList(
                 new Dish("pork", false, 800, Type.MEAT),
@@ -58,10 +66,10 @@ public class CollectStream {
 //        menu.stream().collect(reducing(((o, o2) ->  o.getName()+ o2.getName()))).get();
 //        The above line will not work as one argument that reducing accepts is a  BinaryOperator<T> that’s a BiFunction<T,T,T>
 //        That is 2 arguments and 1 return type all should be same
-//        reducing can take
-//          1) BinaryOperator<T> op -> BiFunction<T,T,T> ---------------- on line 73
-//          2) T identity, BinaryOperator<T> op ----------- on line 67
-//          3) U identity, Function<? extends T, ? extends U> mapper, BinaryOperator<U> op --------- on line 77
+//        reducing can take and returns a collector of type Collector<T, ?, U>
+//          1) BinaryOperator<T> op -> BiFunction<T,T,T> ---------------- on line 81 RETURNS Collector<T, ?, Optional<T>>
+//          2) T identity, BinaryOperator<T> op ----------- on line 67 RETURNS Collector<T,?,T>
+//          3) U identity, Function<? extends T, ? extends U> mapper, BinaryOperator<U> op --------- on line 77 RETURNS Collector<T, ?, U>
 
         System.out.println("--------------GENERAL REDUCING----------------------------");
         int reduceTotCal = menu.stream().collect(reducing(0, Dish::getCalories, (i, j) -> i + j));
@@ -75,12 +83,45 @@ public class CollectStream {
                                 .collect(reducing(0, Dish::getCalories, (c1, c2) -> c1+c2));
         System.out.println("STYLE 3 reducing "+ reduceTotCal3);
 
+        /*groupingBy
+
+        groupingBy(Function<? super T, ? extends K>) classifier) RETURN TYPE Collector<T,  ? , Map<K, List<T>>>
+        Eg Line X
+        Dish - T
+        Dish::getType - Gives a Type which is K (By this time you should understand that this is Function<Dish, Type>)
+        So, return type is surely Map<Type, List<Dish>>
+
+        groupingBy(Function<? super T,? extends K> classifier, Collector<? super T,A,D> downstream) RETURN TYPE Collector<T,?,Map<K,D>>
+        Eg Line X
+        Dish - T
+        Dish:: getType - Gives a type which is K
+        D - RETURN type of second collector , which recursively falls to 1st groupingBy and the return type (D) is derived by that.
+        * */
         System.out.println("-----------------------GROUPING-----------------------------");
+        System.out.println("-------------------GROUP BY DISH TYPE-----------------------");
+        Map<Type, List<Dish>> dishByType = menu.stream().collect(groupingBy(Dish::getType));
+        System.out.println(dishByType);
 
+        System.out.println("-------------------COMPLEX GROUPING-------------------------");
+        Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = menu.stream()
+                .collect(groupingBy(dish -> {
+                    if (dish.getCalories() < 400) return CaloricLevel.DIET;
+                    else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+                    else return CaloricLevel.FAT;
+                }));
 
-
-
-
+        System.out.println(dishesByCaloricLevel);
+        System.out.println("---------------------MULTI LEVEL GROUPING-------------------");
+        Map<Type, Map<CaloricLevel, List<Dish>>> dishByTypeCaloricLevel =
+                menu.stream()
+                .collect(groupingBy(Dish::getType,
+                        groupingBy(dish -> {
+                            if (dish.getCalories() <= 400) return CaloricLevel.DIET;
+                            else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+                            else return CaloricLevel.FAT;
+                        })
+                ));
+        System.out.println(dishByTypeCaloricLevel);
 
 
     }
