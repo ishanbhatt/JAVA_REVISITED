1074072b339a9e2e0d063da36ae9621321b73e03
Ishan Bhatt <ishanbhatt@halwa>
2019-06-11 17:06:13 +0530

    Added an explanation about flatMap on Optional

--- src/Chapter10/OptionalTester.java
+++ src/Chapter10/OptionalTester.java
@@ -9,6 +9,10 @@ public class OptionalTester {
                 .flatMap(Car::getInsurance)
                 .map(Insurance::getName)
                 .orElse("Unknown");
+//      Hopefully you understand why the first two were flatMap and the last one is map
+//      person is an optional, so calling a map(Person::getCar) on person will return Optional<Optional<Car>>
+//      On which you can not call any function, and it would get worse in the chain Optional<Optional<Optional>>
+//      So, like you did on the streams, you need to flatMap it to get Optional<Car> on which you can call getInsurance
     }
 
     public static void main(String[] args) {
