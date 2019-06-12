e9140870021e4d1b7c9b688845a058be71139d18
ishanbhatt <ishan.4525@gmail.com>
2019-06-12 21:19:00 +0530

    Added some refactoring code

--- src/Chapter8/RefactoringJava.java
+++ src/Chapter8/RefactoringJava.java
@@ -1,5 +1,10 @@
 package Chapter8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RefactoringJava {
 
     interface Task {
@@ -20,6 +25,19 @@ public class RefactoringJava {
 //        2 common code patterns that can be refactored to leverage lambda expression
 //        1. Conditional Deferred Execution
 //        2. Execute around
+        System.out.println("----------PEEKING INTO STREAM----------------");
+//          Let's you peek into the stream without ever consuming it
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+
+        numbers.stream()
+                .peek(x -> System.out.println("FROM STREAM "+x))
+                .map(x -> x + 17)
+                .peek(x -> System.out.println("AFTER MAP" +x))
+                .filter(x ->x%2==0)
+                .limit(3)
+                .collect(Collectors.toList());
+
+
 
     }
 }
